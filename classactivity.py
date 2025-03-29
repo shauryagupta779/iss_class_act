7e3e41e3ff60fc46405e010a605d8fc04190c80c
shauryagupta779 <shaurya21feb@gmail.com>
2025-03-29 09:52:45 +0530

    Update classactivity.py

--- classactivity.py
+++ classactivity.py
@@ -1,20 +1,3 @@
-# def find_cube_pairs(target):
-#     solutions = []
-#     max_num = round(target ** (1/3))  
-
-#     for a in range(1, max_num + 1):
-#         for b in range(a, max_num + 1):
-#             if a**3 + b**3 == target:
-#                 solutions.append((a, b))
-#     return solutions
-
-# pairs = find_cube_pairs(1729)
-
-# print("Valid cube pairs for 1729:")
-# for a, b in pairs:
-#     print(f" → {a}³ + {b}³ = {a**3} + {b**3} = 1729")
-
-
 def find_cube_pairs(target):
     #missing colon
     solutions = []  #incorrect semicolon and variable name
